d2bea91b4bedad1b76ef9264dd18fb3b0fa4c2d1
M.Ahmed <mirzaahm34@gmail.com>
2026-06-24 23:47:28 +0500

    Demo pipeline run

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 http.createServer((req, res) => {
     res.writeHead(200);
-    res.end('Hello from Furqan Jenkins Pipeline!');
+    res.end('Hello from Mirza_Ahmed Jenkins Pipeline!');
 }).listen(3000);
 
 console.log('Server Running');
